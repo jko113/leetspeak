b0ade515d4f83fb5fa254f815fcbec3c4de9e4de
Joshua Owens <JKO@Joshuas-MacBook-Pro.local>
2018-04-30 09:38:28 -0400

    first commit

---
+++ leetspeak.py
@@ -0,0 +1,23 @@
+translation = {
+    'A': '4',
+    'E': '3',
+    'G': '6',
+    'I': '1',
+    'O': '0',
+    'S': '5',
+    'T': '7',
+}
+
+sentence = input("What's your sentence? ")
+sentence = sentence.upper()
+
+result = ""
+
+for letter in sentence:
+    if letter in translation:
+        result += translation[letter]
+    else:
+        result += letter
+
+print (result.lower())
+
